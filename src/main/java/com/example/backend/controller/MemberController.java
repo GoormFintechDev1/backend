dc952a310236e319ed1484fae66483a05f88877d
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2024-10-28 11:49:49 +0900

    Feat(AuthService) : 로그인, 회원가입 구현

--- src/main/java/com/example/backend/controller/MemberController.java
+++ src/main/java/com/example/backend/controller/MemberController.java
@@ -1,4 +1,12 @@
 package com.example.backend.controller;
 
 public class MemberController {
+
+
+    //// 정보 수정하기
+    /*@PutMapping("/api/modify/nickname")
+    @PutMapping("/api/modify/phone")
+    @PutMapping("/api/modify/address")*/
+
+
 }
