3c2eb0717161a1610daae5f976d6e29784e8940f
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2024-11-11 14:10:22 +0900

    Feat(member) : 유저 정보 조회 추가

---
+++ src/main/java/com/example/backend/dto/member/myPageDTO.java
@@ -0,0 +1,31 @@
+package com.example.backend.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class myPageDTO {
+    /////////// member
+    // 이름
+    private String name;
+    // 전화번호
+    private String phoneNumber;
+    // 이메일
+    private String email;
+
+
+    /////////// business_registration
+    // 사업자 등록번호
+    private String brNum;
+    // 사업장 주소
+    private String address;
+    // 사업 시작일
+    private LocalDate businessStartDate;
+    // 상호
+    private String companyName;
+
+}
